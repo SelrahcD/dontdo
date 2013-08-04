ca457738a2db1d3f8d4aa67ca902594a6f4262e7
SelrahcD <c.desneuf@gmail.com>
2013-08-04 14:13:46 +0200

    Specify db table

--- app/DontDo/Entities/DontDo.php
+++ app/DontDo/Entities/DontDo.php
@@ -3,5 +3,12 @@
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class DontDo extends Eloquent {
+
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'dontdo';
 	
 }
